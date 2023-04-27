7ecad0a937f1cfcc195868e2a651c199e9c4ad98
DevRall3 <rasmik@edu.umea.se>
2023-04-27 15:14:30 +0200

    tailwind + daisy ui

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -23,6 +24,7 @@ const config = {
 			}
 		}
 	},
+	preprocess: vitePreprocess()
 };
 
 export default config;
