9c8d94f85802034775200d811d9015b2dc773a16
DevRall3 <rasmik@edu.umea.se>
2023-05-11 13:59:47 +0200

    fixed memory game

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,7 @@
+<svelte:head>
+    <title>Home</title>
+</svelte:head>
+
 <script>
     let openBool = false;
 
@@ -48,207 +52,6 @@
         </section>
     </header>
 </main>
-
-<!-- <svelte:head>
-    <title>Home</title>
-</svelte:head>
-
-<main>
-    <nav>
-        <div class="nav-container">
-            <div class="logo">
-                <h1>Rasmus Mikaelsson</h1>
-            </div>
-            <div class="nav-items">
-                <ul>
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/">Projects</a></li>
-                    <li><a href="/">Games</a></li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-    <section class="header">
-        <div class="header-container">
-            <h1>Hello</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, sapiente voluptate a culpa doloribus architecto, eligendi nulla expedita magni amet sequi ratione qui ipsum obcaecati fuga. Molestiae eveniet repudiandae deleniti?</p>
-            <button>Learn More</button>
-        </div>
-    </section>
-</main>
-
-<style>
-
-.nav-container {
-    display: flex;
-    justify-content: space-between;
-    margin: 40px 60px 40px;
-}
-
-.nav-container .logo h1 {
-    font-size: 35px;
-}
-
-ul {
-    display: flex;
-    justify-content: space-evenly;
-
-}
-
-.nav-items {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-}
-
-.nav-items ul li {
-    font-size: 18px;
-    margin: 0 60px;
-}
-
-</style>
-
-<body>
-    <header>
-        <img src="/homeicon.png" alt="Home" class="homeIcon">
-        <ul>
-            <li class="home"><a href="">Home</a></li>
-            <li class="clicker"><a href="/clicker">Clicker</a></li>
-            <li class="todoapp"><a href="/todoapp">To-do App</a></li>
-            <li class="telltale"><a href="/telltale">Telltale</a></li>
-            <li class="loginprototype"><a href="/loginprototype">Login Prototype</a></li>
-        </ul>
-    </header>
-
-    <section>
-        <img src="/stars.png" class="stars" alt="star" style="width:100%; height:70vh;">
-    </section>
-    <div class="box">
-        <h2>Hello!</h2>
-    </div>
-    <section>
-        <img src="/mountains_front.png" class="mountains_front" alt="mountains_front" style="width:100%; height:100%;">
-    
-    </section>
-</body>
-
-<style>
-    .box {
-        position: absolute;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        margin-left: 38%;
-        margin-right: 62%;
-        margin-bottom: 10px;
-        width: 380px;
-        height: 420px;
-        background: linear-gradient(#2b1055, transparent);
-        border-radius: 8px;
-    }
-
-    .box h2 {
-        color: white;
-    }
-
-    .homeIcon {
-        height: 7vh;
-        aspect-ratio: 1/1;
-    }
-
-    .stars {
-        z-index: 0;
-    }
-
-    .mountains_front {
-        z-index: 1;
-    }
-
-    .clicker, .telltale, .todoapp, .home, .todoapp,.loginprototype {
-        transition: transform 0.3s;
-    }
-
-    .clicker:hover, .telltale:hover,.home:hover, .todoapp:hover, .loginprototype:hover {
-        transform: translate(0,5px);
-    }
-
-    header {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        font-family: 'Poppins', sans-serif;
-    }
-
-    section{
-        position: relative;
-        padding:0;
-        margin: 0;
-    }
-
-    body {
-        background: linear-gradient(#2b1055, #7597de);
-        padding: 0;
-        margin: 0;
-    }
-
-    header{
-        position: fixed;
-        top: 0;
-        left: 0;
-        right:0;
-        padding: 30px 100px;
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-
-        z-index: 1000;
-    }
-
-    header .logo {
-        color: white;
-        font-weight: 700;
-        text-decoration: none;
-        font-size: 2em;
-        text-transform: uppercase;
-        letter-spacing: 2px;
-        margin-top: auto;
-        margin-bottom: auto;
-        padding: 15px;
-        border-radius: 20px;
-        transition: all 0.3s;
-    }
-
-    header .logo:hover {
-        transform: translate(0,5px);
-        background: white;
-        color: #2b1055;
-    }
-
-    header ul {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
-
-    header li {
-        list-style: none;
-        margin-left: 20px;
-    }
-
-    header ul li a {
-        text-decoration: none;
-        padding: 15px;
-        color: white;
-        border-radius: 20px;
-        transition: background 0.3s;
-    }
-
-    header ul li a:hover {
-        background: white;
-        color: #2b1055;
-        transform: translate(0,5px);
-    }
-</style> -->
 
 <style>
     main {
@@ -369,7 +172,7 @@ ul {
     /* About Section */
     .about {
         margin: 50vh 10vw 0;
-        padding: 0 10px;
+        padding: 0 25px;
         margin-right: 500px;
         background: rgba(255, 255, 255, 0.1);
         backdrop-filter: blur(15px);
@@ -381,6 +184,7 @@ ul {
     .about-me span {
         color: white;
         font-size: 42px;
+        font-weight: bold;
         text-align: left;
         margin: 10px 0;
     }
@@ -412,7 +216,7 @@ ul {
         }
 
         .about {
-            margin: 45vh 10vw 0;
+            margin: 55vh 0 0;
         }
         .about-me h1,
         .about-me span {
--- src/routes/memory/+page.svelte
+++ src/routes/memory/+page.svelte
@@ -1,21 +1,45 @@
+<svelte:head>
+  <title>Memory Game</title>
+</svelte:head>
+
 <script>
     let cards = [];
-    for (let index = 0; index < 8; index++) {
+    let imgList = ['notrick.gif','moon.png','memorycard.jpg'];
+    for (let index = 0; index < imgList.length*2; index++) {
       cards.push({
-        id: 5, // TODO: unique ids per card card
-        img: "/notrick.gif", // TODO: unique images per card card
-        flipped: false,  // TODO: think
+        id: index, 
+        img: imgList[Math.floor(index/2)],
+        flipped: false,
         completed: false,
       });
     }
+    shuffleArray(cards)
     let flipcount = 0;
     function flip(card) {
+      card.flipped = true;
+      flipcount = flipcount + 1;
       // flip card over if two cards are not already flipped
       // TODO: and card is already not flipped
-      if (card.flipped && flipcount < 2) {
+      if (card.flipped && flipcount <= 2) {
+        for (var i = 0; i < cards.length; i++) {
+          if (cards[i].flipped && !cards[i].completed && cards[i].id !== card.id) {
+            if (cards[i].img === card.img) {
+              card.completed = true;
+              cards[i].completed = true;
+            } else {
+              setTimeout(() => {
+                card.flipped = false;
+                cards[i].flipped = false;
+              }, 1000);
+            }
+            break;
+          }
+        }
+
+    //Do something
         // TODO: Probably do what?
         // flip the cards over after 2s from seeing both cards
-        if (flipcount == 4) {
+        if (flipcount == 2) {
           setTimeout(() => {
             // flip over cards that have not been marked as "completed"
             cards.forEach((card) => {
@@ -23,11 +47,22 @@
             });
             flipcount = 0;
             cards = cards;
-          }, 2000);
+          }, 1500);
         }
         cards = cards;
-      } else {
+      } 
+      
+      else {
         alert("chill");
+      }
+    }
+
+    function shuffleArray(array) {
+      for (var i = array.length - 1; i > 0; i--) {
+          var j = Math.floor(Math.random() * (i + 1));
+          var temp = array[i];
+          array[i] = array[j];
+          array[j] = temp;
       }
     }
   </script>
@@ -62,8 +97,8 @@
     .row {
       display: grid;
       gap: 20px;
-      grid-template-columns: repeat(4, 100px);
-      grid-template-rows: repeat(4, 100px);
+      grid-template-columns: repeat(4, 150px);
+      grid-template-rows: repeat(4, 150px);
     }
     .card {
       border: 1px solid black;
@@ -89,5 +124,6 @@
       backface-visibility: hidden;
       -webkit-backface-visibility: hidden;
       position: absolute;
+      border-radius: 16px;
     }
   </style>
